86ef3a0e7cfdeb865923f13b02cd66638851c146
ShatteredMINT <shatteredmint@gmail.com>
2023-09-03 08:22:50 +0200

    reformat IR class into namespace

--- ir.h
+++ ir.h
@@ -20,17 +20,16 @@ namespace AST{
     class PrototypeAST;
 };
 
-class IR{
-public:
-    static std::unique_ptr<llvm::LLVMContext> Context;
-    static std::unique_ptr<llvm::IRBuilder<>> Builder;
-    static std::unique_ptr<llvm::Module> Module;
-    static std::map<std::string, llvm::Value *> NamedValues;
-    static std::map<std::string, std::unique_ptr<AST::PrototypeAST>> FunctionProtos;
+namespace IR{
+    extern std::unique_ptr<llvm::LLVMContext> Context;
+    extern std::unique_ptr<llvm::IRBuilder<>> Builder;
+    extern std::unique_ptr<llvm::Module> Module;
+    extern std::map<std::string, llvm::Value *> NamedValues;
+    extern std::map<std::string, std::unique_ptr<AST::PrototypeAST>> FunctionProtos;
 
-    static std::unique_ptr<llvm::legacy::FunctionPassManager> FPM;
+    extern std::unique_ptr<llvm::legacy::FunctionPassManager> FPM;
 
-    static void InitializeModuleAndPassManager();
+    void InitializeModuleAndPassManager();
 
-    static llvm::Function * getFunction(std::string Name);
+    llvm::Function * getFunction(std::string Name);
 };
