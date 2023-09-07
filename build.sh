1e153337dd622f4916697819ef9ffbab4fd0f772
Shattered MINT <shatteredmint@gmail.com>
2023-09-07 18:54:48 +0200

    remove build shell script

--- build.sh
+++
@@ -1,1 +0,0 @@
-clang++ -rdynamic -g -O3 src/main.cpp src/lexer.cpp src/parser.cpp src/ast.cpp src/jit.cpp src/ir.cpp $(llvm-config --cxxflags --ldflags --system-libs --libs core orcjit native) -o build/main
