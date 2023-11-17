b95260ef5f8f85aceb20dffcd16f340770a60eb2
ShatteredMINT <shatteredmint@gmail.com>
2023-11-17 10:28:01 +0100

    reformat parser.h

--- src/include/parser.h
+++ src/include/parser.h
@@ -1,37 +1,38 @@
 #pragma once
 
-#include <memory>
 #include <map>
+#include <memory>
 
 namespace AST {
-    class ExprAST;
-    class PrototypeAST;
-    class FunctionAST;
-};
+class ExprAST;
+class PrototypeAST;
+class FunctionAST;
+}; // namespace AST
+
 /**handle everything related to parsing the source code*/
 namespace Parser {
-     std::unique_ptr<AST::ExprAST> ParseExpression();
-     std::unique_ptr<AST::ExprAST> ParseNumberExpr();
-     std::unique_ptr<AST::ExprAST> ParseParenExpr();
-     std::unique_ptr<AST::ExprAST> ParseIdentifierExpr();
-
-     std::unique_ptr<AST::ExprAST> ParsePrimary();
-     /**recursivly evaluetes complex expressions into ordered parts*/
-     std::unique_ptr<AST::ExprAST> ParseBinOpRHS(int ExprPrec, std::unique_ptr<AST::ExprAST> LHS);
-     std::unique_ptr<AST::ExprAST> ParseUnary();
-     std::unique_ptr<AST::ExprAST> ParseIfExpr();
-     std::unique_ptr<AST::ExprAST> ParseForExpr();
-
-     std::unique_ptr<AST::PrototypeAST> ParsePrototype();
-     std::unique_ptr<AST::FunctionAST> ParseDefinition();
-     std::unique_ptr<AST::PrototypeAST> ParseExtern();
-     std::unique_ptr<AST::FunctionAST> ParseTopLevelExpr();
+/**list of precedence of implemented operators*/
+extern std::map<char, int> BinopPrecedence;
 
 
-     /**get precedence of current binary operrator*/
-     int GetTokPrecedence();
+std::unique_ptr<AST::ExprAST> ParseExpression();
+std::unique_ptr<AST::ExprAST> ParseNumberExpr();
+std::unique_ptr<AST::ExprAST> ParseParenExpr();
+std::unique_ptr<AST::ExprAST> ParseIdentifierExpr();
 
+std::unique_ptr<AST::ExprAST> ParsePrimary();
+/**recursivly evaluetes complex expressions into ordered parts*/
+std::unique_ptr<AST::ExprAST> ParseBinOpRHS(int ExprPrec,
+                                            std::unique_ptr<AST::ExprAST> LHS);
+std::unique_ptr<AST::ExprAST> ParseUnary();
+std::unique_ptr<AST::ExprAST> ParseIfExpr();
+std::unique_ptr<AST::ExprAST> ParseForExpr();
 
-     /**list of precedence of implemented operators*/
-     extern std::map<char, int> BinopPrecedence;
-};
+std::unique_ptr<AST::PrototypeAST> ParsePrototype();
+std::unique_ptr<AST::FunctionAST> ParseDefinition();
+std::unique_ptr<AST::PrototypeAST> ParseExtern();
+std::unique_ptr<AST::FunctionAST> ParseTopLevelExpr();
+
+/**get precedence of current binary operrator*/
+int GetTokPrecedence();
+}; // namespace Parser
