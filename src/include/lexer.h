96a2ce430e4cdb06ee74145d2879eb8fcc846d9c
ShatteredMINT <shatteredmint@gmail.com>
2023-11-23 11:37:18 +0100

    reformat lexer.h

--- src/include/lexer.h
+++ src/include/lexer.h
@@ -1,44 +1,45 @@
 #pragma once
 
-#include <string>
 #include <cstdio>
+#include <string>
 
 enum Token {
-    tok_eof = -1,
+  tok_eof = -1,
 
-    //command
-    tok_def = -2,
-    tok_extern =-3,
+  // command
+  tok_def = -2,
+  tok_extern = -3,
 
-    //primary expression
-    tok_identifier = -4,
-    tok_number = -5,
+  // primary expression
+  tok_identifier = -4,
+  tok_number = -5,
 
-    //control
-    tok_if = -6,
-    tok_then = -7,
-    tok_else = -8,
-    tok_for = -9,
-    tok_in = -10,
+  // control
+  tok_if = -6,
+  tok_then = -7,
+  tok_else = -8,
+  tok_for = -9,
+  tok_in = -10,
 
-    // operators
-    tok_binary = -11,
-    tok_unary = -12
+  // operators
+  tok_binary = -11,
+  tok_unary = -12
 };
 
 namespace Lexer {
-    /**contains currently parsed string*/
-    extern std::string IdentifierStr;
-    /**numerical value of current string*/
-    extern double NumVal;
-    /**current token*/
-    extern int CurTok;
+/**contains currently parsed string*/
+extern std::string IdentifierStr;
+/**numerical value of current string*/
+extern double NumVal;
+/**current token*/
+extern int CurTok;
 
-    /** stream from which to read input **/
-    extern FILE * stream;
+/** stream from which to read input **/
+extern FILE *stream;
 
-    /**helper to grab current token and store it*/
-    int getNextToken();
+/**helper to grab current token and store it*/
+int getNextToken();
 
-    int gettok();;
-};
+int gettok();
+;
+}; // namespace Lexer
